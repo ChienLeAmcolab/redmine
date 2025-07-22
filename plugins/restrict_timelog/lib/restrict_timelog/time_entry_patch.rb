95304250d664f84106c94d17e4c00c99a8b35fcf
chienlv <chienlv@amcolab.vn>
2025-07-22 11:03:49 +0700

    custom_validate_setting_fix_3nd

--- plugins/restrict_timelog/lib/restrict_timelog/time_entry_patch.rb
+++ plugins/restrict_timelog/lib/restrict_timelog/time_entry_patch.rb
@@ -1,28 +1,36 @@
+# plugins/restrict_timelog/app/models/restrict_timelog/time_entry_patch.rb
 module RestrictTimelog
   module TimeEntryPatch
     def self.included(base)
       base.class_eval do
-        # khai báo validation kèm điều kiện bỏ qua nếu allowed_tracker?
-        validate :must_not_log_epic_or_story, unless: :allowed_tracker?
-        validate :date_within_issue_range,     unless: :allowed_tracker?
+        # Gộp chung 2 validation chỉ chạy khi tracker không nằm trong allowed
+        with_options unless: :allowed_tracker? do
+          validate :must_not_log_epic_or_story
+          validate :date_within_issue_range
+        end
 
         private
 
-        # method kiểm tra tracker có được phép ngoại lệ không
+        # Tracker nằm trong danh sách cho phép hay không?
         def allowed_tracker?
           ids = Setting.plugin_restrict_timelog['allowed_tracker_ids'] || []
           ids.map(&:to_i).include?(issue.tracker_id)
         end
 
+        # Không cho log nếu tracker không phải Epic/Story và không được phép
         def must_not_log_epic_or_story
-          # ở đây chỉ chạy nếu tracker không nằm trong allowed
-          return if issue_id.blank? || issue.tracker.name.in?(%w[Epic Story])
+          return if issue_id.blank?
+          if issue.tracker.name.in?(%w[Epic Story])
+            # nếu là Epic/Story thì pass
+            return
+          end
           errors.add(:issue, :invalid_tracker)
         end
 
+        # Kiểm tra ngày spent_on nằm trong khoảng [start_date, due_date] của issue
         def date_within_issue_range
-          return if issue_id.blank? || issue.tracker.name.in?(%w[Epic Story])
-          return unless spent_on
+          return if issue_id.blank? || spent_on.nil?
+          # nếu issue không có start_date/due_date thì bỏ qua tương ứng
           if issue.start_date && spent_on < issue.start_date
             errors.add(:spent_on, :before_issue_start, start: issue.start_date.to_s)
           elsif issue.due_date && spent_on > issue.due_date
